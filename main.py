64f9898434fc74112fcef3df8b69f427a1f95f6e
Hrvoje <hrvoje.boskovic@yahoo.com>
2021-02-20 16:09:31 +0100

    Drugi commit

--- main.py
+++ main.py
@@ -1,23 +1,17 @@
-message = "Hello"
-user_name = input("Please enter your name: " )
-user_age = int(input("Please enter your age: " ))
-
-
+message = ("Hello" )
+user_name = input("Please enter your name :")
+user_age = int(input("Please enter your age :"))
 
 
 
 is_user_logged_in = False
 
-if is_user_logged_in :
-   print(message +" "+ user_name )
-   print("Your age is: " + str(user_age - 10))
-   print("User is logged in.")
+if is_user_logged_in:
+     print(message + " " + user_name)
+     print("Your age is: " + str(user_age - 10))
+     print("User is logged in.")
 
 else:
      print("After conditional")
-
-
-
-
 
 
